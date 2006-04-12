bb0f4b3cefda84730d449948522991aa083cd60a
hallski <hallski>
2006-04-12 14:37:15 +0000

    2006-04-12  Mikael Hallendal  <micke@imendio.com>
    	* autogen.sh:
    	* configure.ac:
    	* configure.in:
    	- Bumped the autoconf and automake requirements.
    	- Added support to compile with --with-ssl=gnutls|openssl|no. The
    	  default is to build against gnutls and you must explicitely tell that
    	  you don't want SSL support or configure will fail if you don't have
    	  GnuTLS (or building with --with-ssl=openssl).
    	* loudmouth/Makefile.am:
    	* loudmouth/lm-ssl-openssl.c:
    	- Added an OpenSSL backend. Currently doesn't verify certificates.

--- autogen.sh
+++ autogen.sh
@@ -1,78 +1,191 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-PACKAGE="loudmouth"
+: ${AUTOCONF=autoconf}
+: ${AUTOHEADER=autoheader}
+: ${AUTOMAKE=automake-1.9}
+: ${ACLOCAL=aclocal-1.9}
+: ${LIBTOOLIZE=libtoolize}
+: ${INTLTOOLIZE=intltoolize}
+: ${LIBTOOL=libtool}
+: ${GNOME_DOC_PREPARE=gnome-doc-prepare}
+: ${GTKDOCIZE=gtkdocize}
 
-have_libtool=false
-have_autoconf=false
-have_automake=false
-need_configure_in=false
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-have_gtk_doc=false
-want_gtk_doc=false
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT="loudmouth"
+TEST_TYPE=-f
+FILE=loudmouth/loudmouth.h
+CONFIGURE=configure.ac
 
-if libtool --version < /dev/null > /dev/null 2>&1 ; then
-	libtool_version=`libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-	have_libtool=true
-	case $libtool_version in
-	    1.3*)
-		need_configure_in=true
-		;;
-	esac
-fi
+DIE=0
 
-if autoconf --version < /dev/null > /dev/null 2>&1 ; then
-	autoconf_version=`autoconf --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-	have_autoconf=true
-	case $autoconf_version in
-	    2.13)
-		need_configure_in=true
-		;;
-	esac
-fi
+($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
 
-if $have_libtool ; then : ; else
-	echo;
-	echo "You must have libtool >= 1.3 installed to compile $PACKAGE";
-	echo;
-	exit;
-fi
+(grep "^AC_PROG_INTLTOOL" $srcdir/$CONFIGURE >/dev/null) && {
+  ($INTLTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have \`intltoolize' installed to compile $PROJECT."
+    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.22.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
 
-if grep "^GTK_DOC_CHECK" ./configure.in; then
-	want_gtk_doc=true
-fi
+(grep "^GNOME_DOC_INIT" $srcdir/$CONFIGURE >/dev/null) && {
+  ($GNOME_DOC_PREPARE --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have \`gnome-doc-prepare' installed to compile $PROJECT."
+    #echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.22.tar.gz"
+    #echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
 
-if $want_gtk_doc; then
-	(gtkdocize --version) < /dev/null > /dev/null 2>&1 || {
+if grep "^GTK_DOC_CHECK" $srcdir/$CONFIGURE; then
+	($GTKDOCIZE --version) < /dev/null > /dev/null 2>&1 || {
 	        echo;
 		echo "You need gtk-doc to build $PACKAGE";
 		echo;
 	}
 fi
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have automake installed to compile $PACKAGE";
-	echo;
-	exit;
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.9.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
 }
 
-echo "Generating configuration files for $PACKAGE, please wait...."
-echo;
+(grep "^AM_PROG_LIBTOOL" $CONFIGURE >/dev/null) && {
+  ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have \`libtool' installed to compile $PROJECT."
+    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
 
-if $need_configure_in ; then
-    if test ! -f configure.in ; then
-	echo "Creating symlink from configure.in to configure.ac..."
-	echo
-	ln -s configure.ac configure.in
+if grep "^AM_[A-Z0-9_]\{1,\}_GETTEXT" "$CONFIGURE" >/dev/null; then
+  if grep "sed.*POTFILES" "$CONFIGURE" >/dev/null; then
+    GETTEXTIZE=""
+  else
+    if grep "^AM_GLIB_GNU_GETTEXT" "$CONFIGURE" >/dev/null; then
+      GETTEXTIZE="glib-gettextize"
+      GETTEXTIZE_URL="ftp://ftp.gtk.org/pub/gtk/v2.0/glib-2.0.0.tar.gz"
+    else
+      GETTEXTIZE="gettextize"
+      GETTEXTIZE_URL="ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
     fi
+                                                                                                          
+    $GETTEXTIZE --version < /dev/null > /dev/null 2>&1
+    if test $? -ne 0; then
+      echo
+      echo "**Error**: You must have \`$GETTEXTIZE' installed to compile $PKG_NAME."
+      echo "Get $GETTEXTIZE_URL"
+      echo "(or a newer version if it is available)"
+      DIE=1
+    fi
+  fi
 fi
 
-aclocal $ACLOCAL_FLAGS
-libtoolize --force
-gtkdocize || exit 1
-autoheader
-automake --add-missing
-autoconf
 
-./configure $@ --enable-maintainer-mode --enable-compile-warnings
+if test "$DIE" -eq 1; then
+	exit 1
+fi
 
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+#if test -z "$*"; then
+#	echo "I am going to run ./configure with no arguments - if you wish "
+#        echo "to pass any to it, please specify them on the $0 command line."
+#fi
+
+case $CC in
+*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+for coin in .
+do 
+  dr=`dirname $coin`
+  if test -f $dr/NO-AUTO-GEN; then
+    echo skipping $dr -- flagged as no auto-gen
+  else
+    echo processing $dr
+    macrodirs= #`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
+    ( cd $dr
+      aclocalinclude="$ACLOCAL_FLAGS"
+      for k in $macrodirs; do
+  	if test -d $k; then
+          aclocalinclude="$aclocalinclude -I $k"
+  	##else 
+	##  echo "**Warning**: No such directory \`$k'.  Ignored."
+        fi
+      done
+      if grep "^AM_GLIB_GNU_GETTEXT" $CONFIGURE >/dev/null; then
+	if grep "sed.*POTFILES" $CONFIGURE >/dev/null; then
+	  : do nothing -- we still have an old unmodified $CONFIGURE
+	else
+	  echo "Creating $dr/aclocal.m4 ..."
+	  test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
+	  echo "Running glib-gettextize...  Ignore non-fatal messages."
+	  echo "no" | glib-gettextize --force --copy
+	  echo "Making $dr/aclocal.m4 writable ..."
+	  test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
+        fi
+      fi
+      if grep "^AC_PROG_INTLTOOL" $CONFIGURE >/dev/null; then
+        echo "Running intltoolize..."
+	intltoolize --copy --force --automake
+      fi
+      if grep "^GNOME_DOC_INIT" $CONFIGURE >/dev/null; then
+        echo "Running $GNOME_DOC_PREPARE..."
+	$GNOME_DOC_PREPARE --force --copy || exit 1
+      fi
+      if grep "^AM_PROG_LIBTOOL" $CONFIGURE >/dev/null; then
+	echo "Running $LIBTOOLIZE..."
+	$LIBTOOLIZE --force --copy
+      fi
+      echo "Running $ACLOCAL $aclocalinclude ..."
+      $ACLOCAL $aclocalinclude
+      if grep "^GTK_DOC_CHECK" $CONFIGURE > /dev/null; then
+	echo "Running $GTKDOCIZE..."
+	$GTKDOCIZE
+      fi
+      if grep "^AM_CONFIG_HEADER" $CONFIGURE >/dev/null; then
+	echo "Running $AUTOHEADER..."
+	$AUTOHEADER
+      fi
+      echo "Running $AUTOMAKE --gnu $am_opt ..."
+      $AUTOMAKE --add-missing --gnu $am_opt
+      echo "Running $AUTOCONF ..."
+      $AUTOCONF
+    )
+  fi
+done
+
+conf_flags="--enable-maintainer-mode"
+
+cd "$ORIGDIR"
+
+if test x$NOCONFIGURE = x; then
+  echo Running $srcdir/configure $conf_flags "$@" ...
+  $srcdir/configure $conf_flags "$@" \
+  && echo Now type \`make\' to compile $PROJECT  || exit 1
+else
+  echo Skipping configure process.
+fi
---
+++ loudmouth/lm-ssl-openssl.c
@@ -0,0 +1,207 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2006 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+
+#include <string.h>
+#include <glib.h>
+
+#include "lm-error.h"
+#include "lm-ssl-base.h"
+#include "lm-ssl-internals.h"
+
+#ifdef HAVE_OPENSSL
+
+#include <openssl/ssl.h>
+
+struct _LmSSL {
+	LmSSLBase base;
+
+	SSL_CTX *ctx;
+
+	SSL     *session;
+/*	gnutls_certificate_client_credentials gnutls_xcred;*/
+};
+
+static gboolean       ssl_verify_certificate    (LmSSL       *ssl,
+						 const gchar *server);
+static GIOStatus      ssl_io_status_from_return (LmSSL       *ssl,
+						 gint         error);
+
+static gboolean
+ssl_verify_certificate (LmSSL *ssl, const gchar *server)
+{
+	LmSSLBase *base;
+
+	base = LM_SSL_BASE (ssl);
+
+	/* FIXME: Implement */
+
+	return TRUE;
+}
+
+static GIOStatus
+ssl_io_status_from_return (LmSSL *ssl, gint ret)
+{
+	gint      error;
+	GIOStatus status;
+
+	if (ret > 0) {
+		return G_IO_STATUS_NORMAL;
+	}
+
+	error = SSL_get_error (ssl->session, ret);
+
+	if (error == SSL_ERROR_WANT_READ || error == SSL_ERROR_WANT_WRITE) {
+		status = G_IO_STATUS_AGAIN;
+	} 
+	else if (error == SSL_ERROR_ZERO_RETURN) {
+		status = G_IO_STATUS_EOF;
+	} else {
+		status = G_IO_STATUS_ERROR;
+	}
+
+	return status;
+}
+
+/* From lm-ssl-protected.h */
+
+LmSSL *
+_lm_ssl_new (const gchar    *expected_fingerprint,
+	    LmSSLFunction   ssl_function,
+	    gpointer        user_data,
+	    GDestroyNotify  notify)
+{
+	LmSSL *ssl;
+
+	ssl = g_new0 (LmSSL, 1);
+
+	_lm_ssl_base_init ((LmSSLBase *) ssl,
+			   expected_fingerprint,
+			   ssl_function, user_data, notify);
+
+	return ssl;
+}
+
+void
+_lm_ssl_initialize (LmSSL *ssl) 
+{
+	static gboolean  initialized = FALSE;
+	SSL_METHOD      *meth;
+
+	if (!initialized) {
+		SSL_library_init ();
+
+		/* FIXME: Is this needed when we are not in debug? */
+		SSL_load_error_strings ();
+		initialized = TRUE;
+	}
+
+	meth = SSLv23_method ();
+	ssl->ctx = SSL_CTX_new (meth);
+
+}
+
+gboolean
+_lm_ssl_begin (LmSSL *ssl, gint fd, const gchar *server, GError **error)
+{
+	BIO      *sbio;
+
+	ssl->session = SSL_new (ssl->ctx);
+	sbio = BIO_new_socket (fd, BIO_NOCLOSE);
+	SSL_set_bio (ssl->session, sbio, sbio);
+
+	if (SSL_connect (ssl->session) <= 0) {
+		g_set_error (error, 
+			     LM_ERROR, LM_ERROR_CONNECTION_OPEN,
+			     "*** OpenSSL handshake failed");
+
+		return FALSE;	/* Error */
+	} 
+
+	if (!ssl_verify_certificate (ssl, server)) {
+		g_set_error (error,
+			     LM_ERROR, LM_ERROR_CONNECTION_OPEN,
+			     "*** OpenSSL certificate verification failed");
+		return FALSE;
+	}
+
+	/* FIXME: Check creds */
+	return TRUE; 
+}
+
+GIOStatus
+_lm_ssl_read (LmSSL *ssl, gchar *buf, gint len, gsize *bytes_read)
+{
+	GIOStatus status;
+	gint      b_read;
+
+	*bytes_read = 0;
+	b_read = SSL_read (ssl->session, buf, len);
+
+	status = ssl_io_status_from_return (ssl, b_read);
+
+	if (status == G_IO_STATUS_NORMAL) {
+		*bytes_read = b_read;
+	}
+
+	return status;
+}
+
+gint
+_lm_ssl_send (LmSSL *ssl, const gchar *str, gint len)
+{
+	GIOStatus status;
+	gint      bytes_written;
+
+
+	bytes_written = SSL_write (ssl->session, str, len);
+	status = ssl_io_status_from_return (ssl, bytes_written);
+	
+	while (bytes_written < 0) {
+		if (status != G_IO_STATUS_AGAIN) {
+			return -1;
+		}
+
+		bytes_written = SSL_write (ssl->session, str, len);
+		status = ssl_io_status_from_return (ssl, bytes_written);
+	}
+
+	return bytes_written;
+}
+
+void 
+_lm_ssl_close (LmSSL *ssl)
+{
+	SSL_free (ssl->session);
+	ssl->session = NULL;
+}
+
+void
+_lm_ssl_free (LmSSL *ssl)
+{
+	_lm_ssl_base_free_fields (LM_SSL_BASE (ssl));
+
+	SSL_CTX_free (ssl->ctx);
+
+	g_free (ssl);
+}
+
+#endif /* HAVE_GNUTLS */
